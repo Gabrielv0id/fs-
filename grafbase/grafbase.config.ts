2cbfee05cf7b2d271438bc0f8b0a64e1d10126ec
Gabrielv0id <gabriel1998.fonseca@gmail.com>
2023-07-10 09:12:23 -0300

    feat: implements new model

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -5,14 +5,24 @@ const User = g.model('User', {
   email: g.string().unique(),
   avatarUrl: g.url(),
   debts: g.relation(() => Debt).list().optional(),
+  revenues: g.relation(() => Revenue).list().optional(),
 })
 
-const Debt = g.model('Finance', {
+const Debt = g.model('Debt', {
   title: g.string(),
   description: g.string(),
   value: g.float(),
+  date: g.datetime(),
   type: g.string(),
   owner: g.string(),
+})
+
+const Revenue = g.model('Revenue', {
+  title: g.string(),
+  description: g.string(),
+  value: g.float(),
+  date: g.datetime(),
+  debtor: g.string(),
 })
 
 export default config({
